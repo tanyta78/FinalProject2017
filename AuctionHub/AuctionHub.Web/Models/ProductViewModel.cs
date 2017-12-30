bbfadb013496ecd31a005e31c5e85a2ded7b3e03
Mihail Sotirov <mihail.sotiroff@gmail.com>
2017-12-30 18:58:59 +0200

    ProductViewModel added!

---
+++ AuctionHub/AuctionHub.Web/Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AuctionHub.Web.Models
+{
+    public class ProductViewModel
+    {
+        public ProductViewModel(int id, string name, string description)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
