46723634d247f46fcfdc3a04db213acbe5b3a44d
Mihail Sotirov <mihail.sotiroff@gmail.com>
2018-01-03 22:14:01 +0200

    IPictureService and PictureService files added!

---
+++ AuctionHub/AuctionHub.Services/Implementations/PictureService.cs
@@ -0,0 +1,25 @@
+﻿namespace AuctionHub.Services.Implementations
+{
+    using AuctionHub.Data.Models;
+    using AuctionHub.Services.Contracts;
+    using Microsoft.AspNetCore.Http;
+    using System;
+
+    public class PictureService : IPictureService
+    {
+        public void AddPicture(FormCollection formCollection, int productId, Picture picture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePicture(int? id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void DeleteAllPicturesByProductId(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
